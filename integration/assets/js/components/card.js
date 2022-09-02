f5873113541a3c95c08f6af0a1a05c79f14173c7
César MORO <cesar.moro.dev@gmail.com>
2022-09-02 23:42:28 +0200

    Mise en place d'une fonction setTimeout afin de laisser un délai entre le click sur la carte et l'action déclenchée sur le plateau de jeu + Fonction du bateau a priori complète.

--- integration/assets/js/components/card.js
+++ integration/assets/js/components/card.js
@@ -1,9 +1,10 @@
 export function cardInit() {
     
     //* Déclaration des variables
-    let newCardBtn         = document.querySelector('.cardBtn');
-    let cardsList          = ['boat', 'ocean', 'redOctopus', 'orangeTurtle', 'blueFish', 'greenShark', 'purpleDolphin'];
-
+    let newCardBtn          = document.querySelector('.cardBtn');
+    let cardsList           = ['boat', 'ocean', 'redOctopus', 'orangeTurtle', 'blueFish', 'greenShark', 'purpleDolphin'];
+    let focusedElement      = document.querySelector('.focused');
+    
 
 
     //* Création des interactions utilisateurs
@@ -18,20 +19,52 @@ export function cardInit() {
 
         //* J'incrémente le contenu de ma variable dans ma div
         resultCardDiv.textContent = randomCard;
+        //* Et je laisse un délai d'une seconde entre l'apparition de la carte et son action dans le jeu
+        setTimeout(cardsAction, 1000);
 
-        (function cardsAction() {
+        //* Puis je lance l'action selon le résultat de la carte
+        //* le numéro sorti par randomCard correspond à un index du tableau cardsList.
+        function cardsAction() {
+
+            //* Si le numéro random est égal à 0
+            //* donc si la condition est False puisque 0 vaut False
             if(randomCard == false) {
+
+                //* puisque l'index 0 du tableau correspond au bateau
+                //* alors on déclenche l'action du bateau (element BOAT)
                 (function boatAction() {
-                    
+
+                    //* On récupère la div suivante de même niveau (.boat)
+                    let nextFocusedElement = focusedElement.nextElementSibling;
+                    //* On utilise la div qui contient la classe "focused" 
+                    //* Elle a déjà été récupérée en variable globale
+                    //* Et supprime la classe "focused" de cette div
+                    focusedElement.classList.remove("focused");
+                    //* Puis on ajoute la classe "focused" à la div suivante 
+                    nextFocusedElement.classList.add("focused");
+                    //* ET on attribue la valeur de nextFocusedElement à focusedElement !
+                    //* sinon le code ne fonctionnera plus puisqu'il n'y aura plus d'élément
+                    //* focusedElement contenant la classe "focused"
+                    focusedElement = nextFocusedElement;
                 })();
+
             } else if(randomCard === "1") {
+                (function oceanAction() {
+
+                })();
 
             } else if(randomCard === "2") {
+                (function octopusAction() {
+
+                })();
 
             } else if(randomCard >= "3") {
+                (function animalAction() {
+                    
+                })();
 
             }
-        })()
+        }
 
     }
 }
